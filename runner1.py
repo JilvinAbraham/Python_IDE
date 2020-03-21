adb09455d00b8a1fbeba77928ca9580bfcf8390a
Jilvin Abraham <30723652+JilvinAbraham@users.noreply.github.com>
2020-03-21 15:10:16 +0530

    Add files via upload

---
+++ runner1.py
@@ -0,0 +1,6 @@
+import sys
+sys.stdin=open("Input_1.txt","r")
+sys.stdout=open("Output_1.txt","w+")
+
+t=int(input())
+print(t)
---
+++ runner2.py
@@ -0,0 +1,6 @@
+import sys
+sys.stdin=open("Input_2.txt","r")
+sys.stdout=open("Output_2.txt","w+")
+
+t=int(input())
+print(t)
